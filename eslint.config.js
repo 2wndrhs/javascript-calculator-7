9d2441586f79e66675d47dbfba4d7855dc39ab50
2wndrhs <2wndrhs@gmail.com>
2024-10-22 05:43:24 +0900

    feat: Prettier, ESLint 충돌 방지를 위해 ESLint 설정 수정

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,9 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import pluginJs from "@eslint/js";
-import globals from "globals";
-import path from "path";
-import { fileURLToPath } from "url";
+import { FlatCompat } from '@eslint/eslintrc';
+import pluginJs from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import globals from 'globals';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 // mimic CommonJS variables -- not needed if using CommonJS
 const __filename = fileURLToPath(import.meta.url);
@@ -14,24 +15,25 @@ const compat = new FlatCompat({
 
 export default [
   pluginJs.configs.recommended,
-  ...compat.extends("airbnb-base"),
+  ...compat.extends('airbnb-base'),
+  prettier,
   {
     languageOptions: {
       globals: { ...globals.node, ...globals.jest },
-      ecmaVersion: "latest",
+      ecmaVersion: 'latest',
     },
     rules: {
       // package import를 제외한 모든 import 구문에 대해 확장자를 사용하도록 강제
-      "import/extensions": ["error", "ignorePackages"],
+      'import/extensions': ['error', 'ignorePackages'],
     },
   },
   {
-    files: ["eslint.config.js"],
+    files: ['eslint.config.js'],
     rules: {
       // eslint.config.js 파일에서만 'no-underscore-dangle' 규칙을 비활성화
-      "no-underscore-dangle": "off",
+      'no-underscore-dangle': 'off',
       // package.json 파일이 위치한 프로젝트 루트 디렉토리 경로를 명시
-      "import/no-extraneous-dependencies": ["error", { packageDir: __dirname }],
+      'import/no-extraneous-dependencies': ['error', { packageDir: __dirname }],
     },
   },
 ];
